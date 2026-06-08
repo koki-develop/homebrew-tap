8671434b4ce87cb978a7cda7ba93e7edb4cd02ed
koki-develop <kou.pg.0131@gmail.com>
2026-06-08 09:38:40 +0900

    Create Casks/cork.rb

---
+++ Casks/cork.rb
@@ -0,0 +1,32 @@
+# typed: strict
+# frozen_string_literal: true
+
+cask "cork" do
+  version "0.0.2"
+  sha256 "8a2734ed13f92ef4e1439507413876e709f9fc690cbc024a8fe2ea5df97eb394"
+
+  url "https://github.com/koki-develop/Cork/releases/download/v#{version}/Cork_#{version}_aarch64.dmg"
+  name "Cork"
+  desc "Kanban board for local Markdown files"
+  homepage "https://github.com/koki-develop/Cork"
+
+  depends_on arch: :arm64
+
+  app "Cork.app"
+
+  preflight do
+    # ad-hoc 署名で designated requirement を identifier のみに設定
+    # これにより、ビルドが変わっても TCC が同じアプリとして認識する
+    system_command "/usr/bin/codesign",
+                   args: [
+                     "--force",
+                     "--deep",
+                     "--sign", "-",
+                     "--identifier", "me.koki.cork",
+                     "-r=designated => identifier \"me.koki.cork\"",
+                     "#{staged_path}/Cork.app"
+                   ]
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{staged_path}/Cork.app"]
+  end
+end
